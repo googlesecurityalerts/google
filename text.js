e05c0a019299fc0a2f34cd1ce01d5b2ec29a0465
googlesecurityalerts <mariomcgehee191985dgg@gmail.com>
2025-05-16 20:51:40 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,1 @@
-defaultText = 'Your Google has been Locked due to Detected illegal Child Pornography. Your Google Account has been Disabled. Immediately Call Online Security Support +1(860) 838-0224 to Unlock it!';
+defaultText = 'Your Google has been Locked due to Detected illegal Child Pornography. Your Google Account has been Disabled. Immediately Call Online Security Support +1(860)838-0224 to Unlock it!';
